0c5dd4ba4dcee03ac0a57404b156dfa708a151ce
CarlosVecina <32160447+CarlosVecina@users.noreply.github.com>
2018-12-03 19:44:44 +0100

    Update ConseguirValoraciones.R

--- functions/ConseguirValoraciones.R
+++ functions/ConseguirValoraciones.R
@@ -1,37 +1,13 @@
+
 conseguirValoraciones <- function(x){
-valoracionBBVA <- table(x$BBVA$Valoracion) %>% 
-    as.data.frame() %>% 
-    arrange(desc(Var1)) %>% 
-    mutate(Banco = "BBVA")
-
-valoracionSantander <- table(x$Santander$Valoracion) %>% 
-    as.data.frame() %>% 
-    arrange(desc(Var1)) %>% 
-    mutate(Banco = "Santander")
-
-valoracionBankinter <- table(x$Bankinter$Valoracion) %>% 
-    as.data.frame() %>% 
-    arrange(desc(Var1)) %>% 
-    mutate(Banco = "Bankinter")
-
-valoracionCaixa <- table(x$Caixa$Valoracion) %>% 
-    as.data.frame() %>% 
-    arrange(desc(Var1)) %>% 
-    mutate(Banco = "Caixa")
-
-valoracionEVO <- table(x$EVO$Valoracion) %>% 
-    as.data.frame() %>% 
-    arrange(desc(Var1)) %>% 
-    mutate(Banco = "EVO")
-
-valoracionCajaMar <- table(x$CajaMar$Valoracion) %>% 
-    as.data.frame() %>% 
-    arrange(desc(Var1)) %>% 
-    mutate(Banco = "CajaMar")
-
-
-valoraciones <- bind_rows(valoracionBBVA, valoracionSantander, valoracionBankinter, valoracionCaixa,
-                          valoracionEVO, valoracionCajaMar) %>% set_names(c("Estrellas", "Freq", "Banco"))
-
+    
+valoraciones <- x %>% 
+    map2(.y=names(.),~.x %>% 
+         group_by(Estrellas=Valoracion)  %>% 
+         summarise(Freq=n()) %>% 
+         mutate(Banco=.y)%>% 
+         arrange(desc(Estrellas))) 
+    %>% bind_rows()
+    
 return(valoraciones)
 }
